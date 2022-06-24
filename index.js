6820d6567a9fc5774666b3dc5e97ffe595ea5b4f
LucasF4 <lmateus8673@gmail.com>
2022-06-23 21:07:57 -0300

    teste

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const authorize = require('./authorized_middleware')
-const app = express()
-const port = process.env.PORT || 8080
+const express = require('express');
+const authorize = require('./authorized_middleware');
+const app = express();
+const port = process.env.PORT || 8080;
 
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
